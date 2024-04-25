c8310ebac5439881ecf5291a898e6e5ba2520bf4
Bình Phan <binhpc.21it@vku.udn.vn>
2024-04-25 16:38:24 +0700

    fix:nav-bar

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import AppNavigation from './navigation/AppNavigation';
 import auth from '@react-native-firebase/auth';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import {PermissionsAndroid} from 'react-native';
+import {StatusBar} from 'react-native';
 
 const Stack = createNativeStackNavigator();
 
@@ -17,9 +18,25 @@ function App(): JSX.Element {
     return unsubscribe;
   }, []);
 
+  const theme = {
+    dark: false,
+    colors: {
+      primary: 'rgb(255, 45, 85)',
+      background: 'rgb(242, 242, 242)',
+      card: 'rgb(255, 255, 255)',
+      text: 'rgb(28, 28, 30)',
+      border: 'rgb(199, 199, 204)',
+      notification: 'rgb(255, 69, 58)',
+    },
+  };
   return (
-    <GestureHandlerRootView style={{flex: 1}}>
+    <GestureHandlerRootView style={{flex: 1}} {...{theme}}>
       <NavigationContainer>
+        <StatusBar
+          barStyle="dark-content"
+          backgroundColor="transparent"
+          translucent={false}
+        />
         {user ? AppNavigation() : AuthNavigation()}
       </NavigationContainer>
     </GestureHandlerRootView>
