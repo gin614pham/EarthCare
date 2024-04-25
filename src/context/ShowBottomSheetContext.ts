5d86495305c13fb2013d82c111395022ee594b89
Bình Phan <binhpc.21it@vku.udn.vn>
2024-04-25 10:09:28 +0700

    feature/bottom-bar-and-bottom-sheet

--- src/context/ShowBottomSheetContext.ts
+++ src/context/ShowBottomSheetContext.ts
@@ -1,5 +1,8 @@
 import React from 'react';
 
-const ShowBottomSheetContext = React.createContext({});
+const ShowBottomSheetContext = React.createContext({
+  bottomSheetStatus: true,
+  changeBottomSheetStatus: () => {},
+});
 
 export default ShowBottomSheetContext;
