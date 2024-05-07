4231bad68f9fc5dc3df3d2eb0462b903be10797d
Bình Phan <binhpc.21it@vku.udn.vn>
2024-05-08 02:03:25 +0700

    notificationscreen

--- src/screens/NotificationScreen.tsx
+++ src/screens/NotificationScreen.tsx
@@ -1,53 +1,76 @@
-import {Button, StyleSheet, View} from 'react-native';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {FlatList, StyleSheet, Text, View} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
-import {Text} from 'react-native';
-import notifee, {
-  AndroidImportance,
-  AndroidVisibility,
-} from '@notifee/react-native';
+import UserContext from '../context/UserContext';
+import firestore from '@react-native-firebase/firestore';
 
 function NotificationScreen() {
-  async function onDisplayNotification() {
-    // Request permissions (required for iOS)
-    await notifee.requestPermission();
+  const [notifications, setNotifications] = useState([]);
+  const {user} = React.useContext(UserContext);
 
-    // Create a channel (required for Android)
-    const channelId = await notifee.createChannel({
-      id: 'important',
-      name: 'Important Notifications',
-      importance: AndroidImportance.HIGH,
-      lights: true,
-      vibration: true,
-      visibility: AndroidVisibility.PUBLIC,
-    });
+  useEffect(() => {
+    let subscriber;
 
-    // Display a notification
-    await notifee.displayNotification({
-      title: 'Notification Title',
-      body: 'Main body content of the notification',
-      android: {
-        channelId,
-        smallIcon: 'ic_launcher', // optional, defaults to 'ic_launcher'.
-        // pressAction is needed if you want the notification to open the app when pressed
-        pressAction: {
-          id: 'default',
-        },
-      },
-    });
-  }
+    subscriber = firestore()
+      .collection('notifications')
+      .where('user_id', 'in', ['all', user.uid])
+      .onSnapshot(querySnapshot => {
+        const notifications = [];
+        querySnapshot.forEach(documentSnapshot => {
+          notifications.push({
+            ...documentSnapshot.data(),
+            key: documentSnapshot.id,
+          });
+        });
+        setNotifications(notifications);
+      });
+
+    // Clean up
+    return () => {
+      if (subscriber) {
+        subscriber();
+      }
+    };
+  }, [user]);
+
+  const renderItem = ({item}) => (
+    <View style={styles.item}>
+      <Text style={styles.title}>{item.title}</Text>
+      <Text style={styles.body}>{item.body}</Text>
+    </View>
+  );
 
   return (
-    <SafeAreaView style={{flex: 1}}>
-      <View>
-        <Button
-          title="Display Notification"
-          onPress={() => onDisplayNotification()}
-        />
-      </View>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={notifications}
+        renderItem={renderItem}
+        keyExtractor={item => item.key}
+      />
     </SafeAreaView>
   );
 }
-export default NotificationScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  item: {
+    backgroundColor: '#fff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    borderRadius: 8,
+    elevation: 3,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  body: {
+    fontSize: 16,
+    marginTop: 5,
+  },
+});
+
+export default NotificationScreen;
